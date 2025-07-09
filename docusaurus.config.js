d6136046ab2d7c8e6408f933e755fd168dfb71cc
Adriano Ruseler <ruseler@inep.ufsc.br>
2025-07-09 19:28:47 -0300

    workflow_dispatch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site Title',
-  tagline: 'Triggering deployment with GitHub Actions',
+  tagline: 'Triggering deployment with GitHub Actions (workflow_dispatch)',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
