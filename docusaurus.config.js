83ae6de36c43c9de0f9a03434a813cd12cc39e40
ruseleredu <ruseler@utfpr.edu.br>
2025-07-09 18:35:02 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site Title',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Actions are cool',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
